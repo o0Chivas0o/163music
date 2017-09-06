2d51628558f88c3e3686aa8befa79a5f492d2ca7
Lee <326656669@qq.com>
2017-09-06 21:46:24 +0800

    完成搜索功能

--- home.js
+++ home.js
@@ -4,3 +4,44 @@ $('.tabs').on('click','li',function(e){
     $li.addClass('active').siblings().removeClass('active');
     $('.tabContent').children().eq(index).addClass('active').siblings().removeClass('active')
 });
+
+
+var APP_ID = 'Eix3vKYRx3siO2vV8s30yAPG-gzGzoHsz';
+var APP_KEY = '24qwRAbG2s0ei8lLEwd8z1wi';
+
+AV.init({
+    appId: APP_ID,
+    appKey: APP_KEY
+});
+
+let $olSongs = $('ol#songs');
+
+var query = new AV.Query('Song');
+query.find().then(function (results) {
+    $('div#loading-img').remove();
+    for (let i = 0; i < results.length; i++){
+        let Song = results[i].attributes;
+        let li = `
+                        <li>
+                            <h3>${Song.name}</h3>
+                            <p>
+                                <i class="sq">
+                                    <svg class="icon" aria-hidden="true">
+                                        <use xlink:href="#icon-sq"></use>
+                                    </svg>
+                                </i>
+                                ${Song.singer}
+                                    </p>
+                            <a class="playButton" href="#">
+                                <svg class="icon" aria-hidden="true">
+                                    <use xlink:href="#icon-play"></use>
+                                </svg>
+                            </a>
+                        </li>
+                 `;
+        $olSongs.append(li)
+    }
+}, function (error) {
+    alert('获取歌曲失败')
+});
+
