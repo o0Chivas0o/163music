290986ec7339345a698bf47abcd910653a6cbd3d
Lee <326656669@qq.com>
2017-09-07 21:04:54 +0800

    完成搜索功能

--- home.js
+++ home.js
@@ -1,3 +1,5 @@
+//tab组件
+
 $('.tabs').on('click','li',function(e){
     var $li = $(e.currentTarget);
     var index = $li.index();
@@ -5,6 +7,8 @@ $('.tabs').on('click','li',function(e){
     $('.tabContent').children().eq(index).addClass('active').siblings().removeClass('active')
 });
 
+
+//AV.js
 var APP_ID = 'Eix3vKYRx3siO2vV8s30yAPG-gzGzoHsz';
 var APP_KEY = '24qwRAbG2s0ei8lLEwd8z1wi';
 
@@ -13,15 +17,17 @@ AV.init({
     appKey: APP_KEY
 });
 
-let $olSongs = $('ol#songs');
 
+//首页推荐歌曲
+let $olSongs = $('ol#songs');
 var query = new AV.Query('Song');
 query.find().then(function (results) {
     $('div#loading-img').remove();
-    for (let i = 0; i < results.length; i++){
+    for (var i = 0; i < results.length; i++){
         let Song = results[i].attributes;
         let li = `
                         <li>
+                          <a href="./play.html?id=">
                             <h3>${Song.name}</h3>
                             <p>
                                 <i class="sq">
@@ -31,10 +37,11 @@ query.find().then(function (results) {
                                 </i>
                                 ${Song.singer}
                                     </p>
-                            <a class="playButton" href="#">
+                             <i class="playButton">
                                 <svg class="icon" aria-hidden="true">
                                     <use xlink:href="#icon-play"></use>
                                 </svg>
+                            </i>
                             </a>
                         </li>
                  `;
@@ -44,7 +51,72 @@ query.find().then(function (results) {
     alert('获取歌曲失败')
 });
 
+//热歌榜
+let $olhotSongs = $('ol#hotSongs');
+var query = new AV.Query('Song');
+query.find().then(function (results) {
+    $('div#loading-img').remove();
+    for (var i = 0; i < results.length; i++) {
+        let Song = results[i].attributes;
+        if (i< 9)  {
+            console.log(i)
+            let li = `
+                    <li>
+                            <a href="#">
+                                <div class="number">0${i+1}</div>
+                                <div class="songinfo">
+                                    <div class="content">
+                                        <div class="title">${Song.name}</div>
+                                        <div class="info">
+                                            <i class="sq">
+                                                <svg class="icon" aria-hidden="true">
+                                                    <use xlink:href="#icon-sq"></use>
+                                                </svg>
+                                            </i>
+                                            ${Song.singer}
+                                        </div>
+                                    </div>
+                                    <div class="playbutton">
+                                        <span class="btn"></span>
+                                    </div>
+                                </div>
+                            </a>
+                        </li>
+                 `;
+            $olhotSongs.append(li)
+        }
+        else {
+            let li = `
+                    <li>
+                            <a href="#">
+                                <div class="number">${i+1}</div>
+                                <div class="songinfo">
+                                    <div class="content">
+                                        <div class="title">${Song.name}</div>
+                                        <div class="info">
+                                            <i class="sq">
+                                                <svg class="icon" aria-hidden="true">
+                                                    <use xlink:href="#icon-sq"></use>
+                                                </svg>
+                                            </i>
+                                            ${Song.singer}
+                                        </div>
+                                    </div>
+                                    <div class="playbutton">
+                                        <span class="btn"></span>
+                                    </div>
+                                </div>
+                            </a>
+                        </li>
+                 `;
+            $olhotSongs.append(li)
+        }
+    }
+}, function (error) {
+    alert('获取歌曲失败')
+});
 
+//搜索歌曲
 $('input#search').on('input',function (e) {
     let $input = $(e.currentTarget);
     let value = $input.val().trim();
