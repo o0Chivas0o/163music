e7c7033c75888c7183a47931abf462a7b3496918
Lee <326656669@qq.com>
2017-09-09 00:54:45 +0800

    完成搜索功能 各种点击完成 CSS小修 HTML多余的标签删除

--- home.js
+++ home.js
@@ -80,7 +80,6 @@ query.find().then(function (results) {
     for (var i = 0; i < results.length; i++) {
         let Song = results[i].attributes;
         if (i< 9)  {
-            console.log(i)
             let li = `
                     <li>
                             <a href="#">
@@ -138,15 +137,15 @@ query.find().then(function (results) {
 });
 
 //搜索歌曲
-let timer = null; //设置闹钟
 
+let timer = null; //设置闹钟
 $('input#search').on('input',function (e) {
     if(timer){window.clearTimeout(timer)}
     timer = setTimeout(function(){
         timer = null;
         let $input = $(e.currentTarget);
         let value = $input.val().trim();
-        if(value===''){return}
+        if(value===''){return (close())}
         var query1 = new AV.Query('Song');
         query1.contains('name', value);
         var query2 = new AV.Query('Song');
@@ -156,20 +155,22 @@ $('input#search').on('input',function (e) {
         var query4 = new AV.Query('Song');
         query4.contains('noun', value);
         var query = AV.Query.or(query1, query2,query3,query4);
+        $('.hotResearch').hide();
+        $('.u-svg-empty').addClass('active');
+        $('.searchContent').html(`<h3>搜索“${value}”</h3>`).show();
         query.find().then(function(results){
             $('#searchResult').empty();
             if(results.length === 0){
-                $('#searchResult').html('暂无搜索结果')
+                $('#searchResult').html('<div>暂无搜索结果</div>')
+                $('.searchContent').hide()
             }else{
                 for(var i=0; i<results.length; i++){
                     let song = results[i].attributes;
                     let li = `
-                    <li data-id="${results[i].id}">
-                     <a href="./song.html?id=${results[i].id}">
+                    <li>
+                     <a href="#">
                         <i class="u-svg u-svg-search"></i>
-                        <span>
-                         ${song.name} - ${song.singer}
-                        </span>
+                        <span class="songName">${song.name}</span>
                      </a>
                     </li>
                     `;
@@ -177,5 +178,124 @@ $('input#search').on('input',function (e) {
                 }
             }
         })
-    },400)
+    },300)
 });
+
+//封装close
+var close = function(){
+    $('#search').val('');
+    $('.u-svg-empty').removeClass('active');
+    $('#searchResult').empty();
+    $('.hotResearch').show();
+    $('.searchContent').hide();
+    $('#hot-list').hide()
+};
+
+//清空input按钮
+$('.u-svg-empty').on('click', function () {
+        close()
+});
+
+//点击热门搜索
+$('.hotResearch> ul').on('click', 'li', function (e) {
+    let $ul = $(e.currentTarget);
+    $('#search').val($ul.text());
+    var value = $('#search').val();
+    var query1 = new AV.Query('Song');
+    query1.contains('name', value);
+    var query2 = new AV.Query('Song');
+    query2.contains('singer', value);
+    var query = AV.Query.or(query1, query2);
+    $('.hotResearch').hide();
+    $('.u-svg-empty').addClass('active');
+    query.find().then(function (results){
+        for (var i = 0; i<results.length; i++){
+            $('#hot-list').empty();
+            $('#hot-list').show();
+            let song = results[i].attributes;
+            let li =`
+                    <li>
+                            <a href="./song.html?id=${results[i].id}">
+                                <div class="song-info">
+                                    <div class="hot-cont">
+                                        <div class="hot-title">${song.name}</div>
+                                        <div class="hot-info">
+                                            <i class="sq">
+                                                <svg class="icon" aria-hidden="true">
+                                                    <use xlink:href="#icon-sq"></use>
+                                                </svg>
+                                            </i>
+                                            ${song.singer} - ${song.album}
+                                        </div>
+                                    </div>
+                                    <div class="play-button">
+                                        <span class="btn"></span>
+                                    </div>
+                                </div>
+                            </a>
+                        </li>
+                 `;
+            $('#hot-list').append(li)
+        }
+    }, function (error) {
+    })
+});
+
+//显示搜索
+$('#searchResult').on('click','li',function() {
+    $('#search').val($('span.songName').text());
+    var value = $('span.songName').text();
+    var query = new AV.Query('Song');
+    query.contains('name', value);
+    $('.searchContent').hide();
+    query.find().then(function (results) {
+        for (var i = 0; i < results.length; i++) {
+            $('#searchResult').hide()
+            let song = results[i].attributes;
+            let li = `
+                        <li>
+                            <a href="./song.html?id=${results[i].id}">
+                                <div class="song-info">
+                                    <div class="hot-cont">
+                                        <div class="hot-title">${song.name}</div>
+                                        <div class="hot-info">
+                                            <i class="sq">
+                                                <svg class="icon" aria-hidden="true">
+                                                    <use xlink:href="#icon-sq"></use>
+                                                </svg>
+                                            </i>
+                                            ${song.singer} - ${song.album}
+                                        </div>
+                                    </div>
+                                    <div class="play-button">
+                                        <span class="btn"></span>
+                                    </div>
+                                </div>
+                            </a>
+                        </li>
+                     `
+            $('#hot-list').append(li)
+        }
+    }, function (error) {
+        console.log(error)
+    })
+});
+
+
+
+
+
+/*热门歌曲列表*/
+$('.hot-list > ol').on('click', 'li', function () {
+    $('#search').val($(this).text());
+    hot()
+});
+
+//封装hot
+var hot = function () {
+     $('.hot-list').hide();
+     $('.hotResearch').hide();
+     $('.u-svg-empty').addClass('active');
+     $('#searchResults').empty();
+     var value = $('#search').val();
+};
